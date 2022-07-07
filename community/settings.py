cffbabc892cecc96a40bade708ea687715abff5e
wazeer1 <88919483+wazeer1@users.noreply.github.com>
2022-07-06 21:38:18 -0700

    Update settings.py

--- community/settings.py
+++ community/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'community.urls'
@@ -82,6 +83,8 @@ TEMPLATES = [
         },
     },
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 WSGI_APPLICATION = 'community.wsgi.application'
 
